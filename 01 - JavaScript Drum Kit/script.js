f09a03e415525b788e8c09cb33a6fb459c49fc5f
Subhajit Roy <roysubhajit8697@gmail.com>
2022-02-02 20:41:46 +0530

    initial commit

---
+++ 01 - JavaScript Drum Kit/script.js
@@ -0,0 +1,20 @@
+const playDrum = (e) => {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    if(!audio) return;
+    audio.currentTime = 0; //rewind to the start
+    audio.play();
+
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    key.classList.add('playing');
+
+}
+
+function removeTransition(e) {
+    if(e.propertyName !== "transform") return; //skip it if it's not a transform
+    this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener("keydown", playDrum);
