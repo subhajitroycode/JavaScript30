0caeaa755d124823940947bcb96f7ab671dc5822
Subhajit Roy <roysubhajit8697@gmail.com>
2022-02-08 21:33:52 +0530

    script file added

---
+++ 04 - Array Cardio Day 1/script.js
@@ -0,0 +1,93 @@
+// Get your shorts on - this is an array workout!
+// ## Array Cardio Day 1
+
+// Some data we can work with
+
+const inventors = [
+    { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+    { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+    { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+    { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+    { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+    { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+    { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+    { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
+    { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
+    { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
+    { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
+    { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
+    ];
+
+const people = [
+    'Bernhard, Sandra', 'Bethea, Erin', 'Becker, Carl', 'Bentsen, Lloyd', 'Beckett, Samuel', 'Blake, William', 'Berger, Ric', 'Beddoes, Mick', 'Beethoven, Ludwig',
+    'Belloc, Hilaire', 'Begin, Menachem', 'Bellow, Saul', 'Benchley, Robert', 'Blair, Robert', 'Benenson, Peter', 'Benjamin, Walter', 'Berlin, Irving',
+    'Benn, Tony', 'Benson, Leana', 'Bent, Silas', 'Berle, Milton', 'Berry, Halle', 'Biko, Steve', 'Beck, Glenn', 'Bergman, Ingmar', 'Black, Elk', 'Berio, Luciano',
+    'Berne, Eric', 'Berra, Yogi', 'Berry, Wendell', 'Bevan, Aneurin', 'Ben-Gurion, David', 'Bevel, Ken', 'Biden, Joseph', 'Bennington, Chester', 'Bierce, Ambrose',
+    'Billings, Josh', 'Birrell, Augustine', 'Blair, Tony', 'Beecher, Henry', 'Biondo, Frank'
+    ];
+    
+// Array.prototype.filter()
+// 1. Filter the list of inventors for those who were born in the 1500's
+const legends = inventors.filter(people => people.year >= 1500 && people.year < 1600);
+console.table(legends);
+
+// Array.prototype.map()
+// 2. Give us an array of the inventors first and last names
+const legendNames = inventors.map(inventor => {
+    return inventor.first + ' ' + inventor.last;
+});
+console.log(legendNames);
+
+// Array.prototype.sort()
+// 3. Sort the inventors by birthdate, oldest to youngest
+const sortBirthdays = inventors.sort((firstPerson, lastPerson) => firstPerson.year > lastPerson.year ? 1 : -1);
+console.table(sortBirthdays);
+
+// Array.prototype.reduce()
+// 4. How many years did all the inventors live all together?
+const totalYears = inventors.reduce((accu, curr) => {
+    return accu + (curr.passed - curr.year);
+}, 0);
+console.log(totalYears);
+
+// 5. Sort the inventors by years lived
+const yearsLived = inventors.sort(function(fPerson, lPerson) {
+    const fPersonLived = fPerson.passed - fPerson.year;
+    const lPersonLived = lPerson.passed - lPerson.year;
+
+    if(fPersonLived > lPersonLived)
+        return -1;
+    else
+        return 1;
+});
+console.table(yearsLived);
+
+// 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
+// https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+// let category = document.querySelector('.mw-category');
+// let links = Array.from(category.querySelectorAll('a')); //Array.from change nodelists into an array
+// let de = links.map(link => link.textContent).filter(streetName => streetName.includes('de')); //filter out the list of streetname which contains 'de' word
+
+
+// 7. sort Exercise
+// Sort the people alphabetically by last name
+const lastNameSort = people.sort((lastOne, nextOne) => {
+    const [aLastName, aFirstName] = lastOne.split(', ');
+    const [bLastName, bFirstName] = nextOne.split(', ');
+
+    return aLastName > bLastName ? 1 : -1;
+});
+console.table(lastNameSort);
+
+// 8. Reduce Exercise
+// Sum up the instances of each of these
+const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];
+const transportation = data.reduce((obj, item) => {
+    if(!obj[item])
+        obj[item] = 0;
+    
+    obj[item]++;
+    return obj;
+}, {});
+
+console.log(transportation);
