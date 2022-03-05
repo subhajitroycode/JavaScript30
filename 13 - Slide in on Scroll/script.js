ea345884b41f5a8420c87b75f37820c158315cbb
Subhajit Roy <roysubhajit8697@gmail.com>
2022-03-05 19:52:11 +0530

    identation fixed

--- 13 - Slide in on Scroll/script.js
+++ 13 - Slide in on Scroll/script.js
@@ -13,23 +13,23 @@ function debounce(func, wait = 20, immediate = true) {
     };
   }
 
-  const sliderImages = document.querySelectorAll(".slide-in");
+const sliderImages = document.querySelectorAll(".slide-in");
 
-  function checkSlide(e) {
-      sliderImages.forEach(sliderImage => {
-        // half way through the image
-        const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
-        // bottom of the image
-        const imageBottom = sliderImage.offsetTop + sliderImage.height;
+function checkSlide(e) {
+  sliderImages.forEach(sliderImage => {
+    // half way through the image
+    const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+    // bottom of the image
+    const imageBottom = sliderImage.offsetTop + sliderImage.height;
 
-        const isHalfShown = slideInAt > sliderImage.offsetTop;
-        const isNotScrolledPast = window.scrollY < imageBottom;
-        // check if the image is half shown and not scrolled past
-        if(isHalfShown && isNotScrolledPast)
-          sliderImage.classList.add('active');
-        else
-          sliderImage.classList.remove('active');
-      })
-  }
+    const isHalfShown = slideInAt > sliderImage.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    // check if the image is half shown and not scrolled past
+    if(isHalfShown && isNotScrolledPast)
+      sliderImage.classList.add('active');
+    else
+      sliderImage.classList.remove('active');
+  })
+}
 
 window.addEventListener("scroll", debounce(checkSlide));
