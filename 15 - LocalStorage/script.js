7335ec255d7b0bd9020eb10bfd4124736a3fe9a0
Subhajit Roy <roysubhajit8697@gmail.com>
2022-03-07 20:38:13 +0530

    delete, select all and unselect all buttons added

--- 15 - LocalStorage/script.js
+++ 15 - LocalStorage/script.js
@@ -1,6 +1,6 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
-const items = JSON.parse(localStorage.getItem('items')) ||[];
+const items = JSON.parse(localStorage.getItem('items')) || [];
 
 function addItem(e) {
     e.preventDefault();    //Prevent the page from reloading or submitting the form
@@ -19,7 +19,7 @@ function addItem(e) {
 const populateList = (plates = [], plateList) => {
     plateList.innerHTML = plates.map((plate, index) => {
         return `
-            <li>
+            <li class="food">
                 <input type="checkbox" data-index=${index} id="item${index}" ${plate.done ? "checked" : ""}>
                 <label for="item${index}">${plate.text}</label>
             </li>
@@ -33,6 +33,32 @@ function toggleDone(e) {
     items[index].done = !items[index].done;
     localStorage.setItem("items", JSON.stringify(items));
     populateList(items, itemsList);
+}
+
+const removeAll = () => {
+    localStorage.clear();
+    itemsList.innerHTML = "";
+    while(items.length > 0) {
+        items.pop();            //remove everything from the array
+    }
+}
+
+const selectAll = () => {
+    const checkboxes = itemsList.querySelectorAll("input[type=checkbox]");
+    checkboxes.forEach((checkbox, index) => {
+        checkbox.checked = true;
+        items[index].done = true;
+    });
+    localStorage.setItem("items", JSON.stringify(items));
+}
+
+const unselectAll = () => {
+    const checkboxes = itemsList.querySelectorAll("input[type=checkbox]");
+    checkboxes.forEach((checkbox, index) => {
+        checkbox.checked = false;
+        items[index].done = false;
+    });
+    localStorage.setItem("items", JSON.stringify(items));
 }
 
 addItems.addEventListener("submit", addItem);
